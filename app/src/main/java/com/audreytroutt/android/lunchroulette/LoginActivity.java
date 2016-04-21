69671c34de220aeec7d9e8344ef0b2868a729914
Audrey Troutt <audrey.troutt@gmail.com>
2016-04-21 18:13:26 -0400

    Stay logged in if you are already logged in (YAY)

--- app/src/main/java/com/audreytroutt/android/lunchroulette/LoginActivity.java
+++ app/src/main/java/com/audreytroutt/android/lunchroulette/LoginActivity.java
@@ -1,58 +1,27 @@
 package com.audreytroutt.android.lunchroulette;
 
-import android.animation.Animator;
-import android.animation.AnimatorListenerAdapter;
-import android.annotation.TargetApi;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.support.annotation.NonNull;
-import android.support.design.widget.Snackbar;
+import android.os.Bundle;
 import android.support.v4.content.res.ResourcesCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-
-import android.content.CursorLoader;
-import android.content.Loader;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.AsyncTask;
-
-import android.os.Build;
-import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.text.TextUtils;
 import android.util.Log;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.inputmethod.EditorInfo;
-import android.widget.ArrayAdapter;
-import android.widget.AutoCompleteTextView;
-import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.audreytroutt.android.lunchroulette.data.FirebaseUtility;
 import com.firebase.client.AuthData;
 import com.firebase.client.Firebase;
 import com.firebase.client.FirebaseError;
+import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import butterknife.Bind;
 import butterknife.ButterKnife;
-
-import static android.Manifest.permission.READ_CONTACTS;
 
 /**
  * A login screen that offers login via email/password.
@@ -87,6 +56,14 @@ public class LoginActivity extends AppCompatActivity {
                 Toast.makeText(getApplicationContext(), "Error authenticating with Twitter: " + exception.getMessage(), Toast.LENGTH_LONG).show();
             }
         });
+
+        TwitterSession session = Twitter.getInstance().core.getSessionManager().getActiveSession();
+        if (session != null) {
+            loginButton.setVisibility(View.INVISIBLE);
+            String msg = "Welcome Back @" + session.getUserName() + "!";
+            Toast.makeText(getApplicationContext(), msg, Toast.LENGTH_LONG).show();
+            FirebaseUtility.getInstance().authenticateTwitterSessionWithResultHandler(session, new FirebaseAuthenticationHandler());
+        }
     }
 
     @Override
